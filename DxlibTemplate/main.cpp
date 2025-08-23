9724f797b7c9e0f8914cb109ec20d543a4158702
Aryu <suwako_a-u-@hotmail.co.jp>
2025-08-23 14:55:42 +0900

    コード整理

--- DxlibTemplate/main.cpp
+++ DxlibTemplate/main.cpp
@@ -2,6 +2,7 @@
 #include "SceneManager.h"
 #include "Ids.h"
 #include "InputManager.h"
+#include "FramePacer.h"
 #include "Logger.h"
 #include <exception>
 #include <windows.h>
@@ -10,17 +11,15 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ 
     Logger::I().init("logs/game.log", 3 * 1024 * 1024, Logger::Level::Debug);
     SceneManager mgr;
     mgr.startWith(SceneID::Title);
-    int counter = 0;
     try {
         ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK);
         while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+			FramePacer::GetInstance().Update();
 			InputManager::GetInstance().Update();
             mgr.updateAndDraw();
-            LOG_INFO("COUNTER : " + counter);
-            counter++;
+			FramePacer::GetInstance().Wait();
         }
-    }
-    catch (const std::exception& e) {
+    } catch (const std::exception& e) {
         MessageBoxA(NULL, e.what(), "Error", MB_OK | MB_ICONERROR);
     }
     DxLib_End(); // DXÉâÉCÉuÉâÉäèIóπèàóù
